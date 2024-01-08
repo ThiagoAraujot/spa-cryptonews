15629d0bd77da05a243d407311f4d75d3925bbfa
thiago araujo <devthiagoaraujo@gmail.com>
2024-01-08 14:15:45 -0300

    feat: textlimited component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,35 @@
+import { useEffect, useState } from "react";
+
 import { Card } from "../../components/Card/Card";
 import { Navbar } from "../../components/Navbar/Navbar";
-import { news } from "../../Datas.js";
+import { getAllNews } from "../../services/newsService.js";
 import { HomeBody } from "./HomeStyled.js";
 
 export default function Home() {
+  const [news, setNews] = useState([]);
+
+  async function findAllNews() {
+    const response = await getAllNews();
+    setNews(response.data.results);
+  }
+
+  useEffect(() => {
+    findAllNews();
+  }, [])
+
   return (
     <>
       <Navbar />
       <HomeBody>
-        {news.map((item, index) => (
-          <Card key={index} news={item} />
+        {news.map((item) => (
+          <Card
+            key={item.id}
+            title={item.title}
+            text={item.text}
+            banner={item.banner}
+            likes={item.likes.length}
+            comments={item.comments.length}
+          />
         ))}
       </HomeBody>
     </>
