098ebf464490b30732ad3946be942b90fdd8b37f
thiago araujo <devthiagoaraujo@gmail.com>
2024-01-08 13:20:38 -0300

    feat: cards created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
+import { Card } from "../../components/Card/Card";
 import { Navbar } from "../../components/Navbar/Navbar";
+import { news } from "../../Datas.js";
+import { HomeBody } from "./HomeStyled.js";
 
 export default function Home() {
-    return (
-        <>
-        <Navbar/>
-        </>
-    )
-} 
+  return (
+    <>
+      <Navbar />
+      <HomeBody>
+        {news.map((item, index) => (
+          <Card key={index} news={item} />
+        ))}
+      </HomeBody>
+    </>
+  );
+}
