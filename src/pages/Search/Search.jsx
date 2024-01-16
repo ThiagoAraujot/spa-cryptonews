d83a1b1406c3eaf0caa2b8fbce19e611f5bfba94
thiago araujo <devthiagoaraujo@gmail.com>
2024-01-16 13:28:31 -0300

    feat: search layout

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,7 +1,52 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { searchNews } from "../../services/newsService";
+import { Card } from "../../components/Card/Card.jsx";
+import { ContainerResults, SearchNews, TextResults } from "./SearchStyled.js";
 
 export function Search() {
-  const {title} = useParams();
+  const { title } = useParams();
+  const [news, setNews] = useState([]);
 
-  return <h1>{title}</h1>;
+  async function search() {
+    try {
+      const newsApi = await searchNews(title);
+      setNews(newsApi.data.results);
+    } catch (err) {
+      console.log(err);
+      setNews([]);
+    }
+  }
+
+  useEffect(() => {
+    search();
+  }, [title]);
+
+  return (
+    <ContainerResults>
+      <TextResults>
+        <span>
+          {news.length
+            ? `Encontramos ${news.length} ${
+                news.length > 1 ? "resultados" : "resultado"
+              } para: `
+            : "Não encontramos resultados para:"}
+        </span>
+        <h2>{title}</h2>
+      </TextResults>
+
+      <SearchNews>
+        {news.map((item) => (
+          <Card
+            key={item.id}
+            title={item.title}
+            text={item.text}
+            banner={item.banner}
+            likes={item.likes}
+            commments={item.comments}
+          />
+        ))}
+      </SearchNews>
+    </ContainerResults>
+  );
 }
--- src/services/newsService.js
+++ src/services/newsService.js
@@ -8,6 +8,11 @@ export function getAllNews() {
 }
 
 export function getTopNews() {
-  const response = axios.get(`${baseURL}/news/top`); 
+  const response = axios.get(`${baseURL}/news/top`);
+  return response;
+}
+
+export function searchNews(title) {
+  const response = axios.get(`${baseURL}/news/search?title=${title}`);
   return response;
 }
